4e5c675f06fd13e161ee2b382ffca8c3a5842b98
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-08-01 19:56:08 -0300

    começa interfaces

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 app.use(express.json());
-app.get('/teste', (req, res) => res.status(200).send('teste'));
+app.get('/teste', (req, res) => res.status(200).send('oiii'));
 
 export default app;
