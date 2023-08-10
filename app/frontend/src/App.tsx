d3fcefba3994013d107ca146f8b10295b6e39014
Gustavo Filipe Tardin de Paula <gustavofilipetardin@outlook.com>
2023-08-10 17:27:49 -0300

    Login development (#11)
    * feat: cria page Login e toda sua lógica sem envolver o banco de dados, gerenciando estatos e verificações de input
    
    * refactor: Refatora page Login.tsx deixando o código mais limpo, e já começa o conceito de rotas no arquivo App

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+
 function App() {
   return (
-    <h1>Configuração inicial</h1>
-  )
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- app/frontend/src/Pages/Login.tsx
+++ app/frontend/src/Pages/Login.tsx
@@ -1,3 +1,40 @@
-export default function Login() {
+import { ChangeEvent, useState } from 'react';
 
+export default function Login() {
+  const [user, userSetter] = useState('');
+  const [password, passwordSetter] = useState('');
+
+  const handleButton = (): boolean => {
+    return !(user.length >= 3 && password.length >= 4);
+  };
+
+  const tryLogin = (event: React.FormEvent): void => {
+    event.preventDefault();
+  };
+
+  return (
+    <form action="">
+      <label htmlFor="user">Nome de Usuário</label>
+      <input
+        type="text"
+        id="user"
+        value={ user }
+        onChange={ (e: ChangeEvent<HTMLInputElement>) => userSetter(e.target.value) }
+      />
+      <label htmlFor="password">Senha</label>
+      <input
+        type="password"
+        id="password"
+        value={ password }
+        onChange={ (e: ChangeEvent<HTMLInputElement>) => passwordSetter(e.target.value) }
+      />
+      <button
+        disabled={ handleButton() }
+        onClick={ tryLogin }
+      >
+        Entrar
+
+      </button>
+    </form>
+  );
 }
