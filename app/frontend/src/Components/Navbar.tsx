c9a0c18afa6632dbe8d73b838a439077f277b9c5
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-08-17 02:08:10 -0300

    feat: coloca css basico no NavBar e o adiciona  na página administrativa

--- app/frontend/src/Components/Navbar.tsx
+++ app/frontend/src/Components/Navbar.tsx
@@ -1,39 +1,44 @@
+import '../Styles/Navbar.css';
+
 function NavBar() {
   const credential = localStorage.getItem('credential');
   const isAdmin = credential === 'Administrador';
-  const storeAcess = isAdmin || credential === 'lojista';
-  const stockAcess = isAdmin || credential === 'Estoquista';
+  const storeAccess = isAdmin || credential === 'lojista';
+  const stockAccess = isAdmin || credential === 'Estoquista';
+
   return (
-    <ul>
-      {(storeAcess) && (
+    <div className="nav-container">
+      <ul className="nav-list">
+        {storeAccess && (
+          <li>
+            <a href="/novo-pedido">Novo Pedido</a>
+          </li>
+        )}
         <li>
-          <a href="/novo-pedido">Novo Pedido</a>
-        </li>)}
-      <li>
-        <a href="/produtos-em-falta">Produtos em falta</a>
-      </li>
-      {stockAcess && (
-        <>
-          <li>
-            <a href="/pedidos-pendentes">Pedidos pendentes</a>
-          </li>
-          <li>
-            <a href="/reposição-de-produtos">Reposição de produtos</a>
-          </li>
-        </>
-      )}
-      {
-            (isAdmin) && (
-              <>
-                <li>
-                  <a href="/painel-administrativo">Painel Administrativo</a>
-                </li>
-                <li>
-                  <a href="/valuation">Valuation</a>
-                </li>
-              </>)
-}
-    </ul>
+          <a href="/produtos-em-falta">Produtos em falta</a>
+        </li>
+        {stockAccess && (
+          <>
+            <li>
+              <a href="/pedidos-pendentes">Pedidos pendentes</a>
+            </li>
+            <li>
+              <a href="/reposição-de-produtos">Reposição de produtos</a>
+            </li>
+          </>
+        )}
+        {isAdmin && (
+          <>
+            <li>
+              <a href="/painel-administrativo">Painel Administrativo</a>
+            </li>
+            <li>
+              <a href="/valuation">Valuation</a>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
   );
 }
 
--- app/frontend/src/Pages/AdminPanel.tsx
+++ app/frontend/src/Pages/AdminPanel.tsx
@@ -1,6 +1,14 @@
+import NavBar from '../Components/Navbar';
+
 function AdminPanel() {
   return (
-    <h1>Painel Administrativo</h1>
+    <div>
+      <header>
+        <NavBar />
+      </header>
+      <h1>Painel Administrativo</h1>
+
+    </div>
   );
 }
 
