96dd88b4a1a3461f873894b38c6b1b253a4ee36d
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-08-30 03:24:46 -0300

    Fix: conserta schema Joi para newStore e tira console.log dos arquivos

--- app/backend/src/api/Utils/JoiSchemas/newStoreSchema.ts
+++ app/backend/src/api/Utils/JoiSchemas/newStoreSchema.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const newStoreSchema = Joi.object({
   name: Joi.string().min(4).required(),
   sellers: Joi.array().items(Joi.string()),
-  logoPath: Joi.string(),
+  logoPath: Joi.string().allow(null),
 });
 
 export default newStoreSchema;
