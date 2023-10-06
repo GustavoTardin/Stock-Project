121f0782042692c40c86754a33295d7042b7dfe5
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-10-06 15:35:14 -0300

    fix: Corrige mensagem de erro c informação errada

--- app/backend/src/api/Contracts/zod/schemas/newStoreSchema.ts
+++ app/backend/src/api/Contracts/zod/schemas/newStoreSchema.ts
@@ -4,7 +4,7 @@ import '../zodConfig';
 const newStoreSchema = z.object({
   name: z
     .string()
-    .min(4, { message: 'Nome da loja deve ter no mínimo 3 caracteres' })
+    .min(4, { message: 'Nome da loja deve ter no mínimo 4 caracteres' })
     .refine((value) => value.trim().length > 0, {
       message: 'Nome de usuário é obrigatório',
     }),
