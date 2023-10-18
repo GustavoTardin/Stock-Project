394a83bec77aa3a9bc590fe4210005174ebe8da8
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-10-18 15:21:15 -0300

    fix: Fix ts-node and ts-node-dev versions that were causing TS errors when transpiling

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,16 +1,17 @@
-/* import 'dotenv/config';
+import 'dotenv/config';
 import app from './app';
-import connectToDatabase from './api/Models/Connection';
 
 const PORT = process.env.PORT || 3009;
-connectToDatabase()
-  .then(() => {
+
+function serverUp() {
+  try {
     app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log('Connection with database generated an error:\r\n');
     console.error(error);
     console.log('\r\nServer initialization cancelled');
     process.exit(0);
-  });
-*/
+  }
+}
+
+serverUp();
