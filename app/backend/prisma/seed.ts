746648a757881174db088780e6e84c7e178cc9ac
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-10-18 19:19:11 -0300

    fix: initialize and config prisma correctly, and already create users seeders

--- app/backend/prisma/seed.ts
+++ app/backend/prisma/seed.ts
@@ -1,18 +1,17 @@
-import { PrismaClient } from '@prisma/client';
-import users from './seeds/users';
+import { PrismaClient } from "@prisma/client";
+import users from "./seeds/users";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // eslint-disable-next-line prefer-const
-  for (let user of users) {
-    await prisma.User.create({
-      data: user,
-    });
-  }
+    for (let user of users) {
+        await prisma.user.create({
+            data: user
+        })
+    }
 }
 
-main().catch((e) => {
-  console.log(e);
-  process.exit(1);
+main().catch(e => {
+    console.log(e); 
+    process.exit(1);
 }).finally(() => prisma.$disconnect());
