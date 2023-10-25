c9a6804cbabd770e2bdb1a1827876ebabdca241f
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-10-25 13:11:39 -0300

    aa

---
+++ app/backend/prisma/migrations/20231023200745_test/migration.sql
@@ -0,0 +1,146 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "collectionId" INTEGER NOT NULL,
+    "sizeId" INTEGER NOT NULL,
+    "designId" INTEGER NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "cost" DECIMAL(65,30) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductCategory" (
+    "id" SERIAL NOT NULL,
+    "categoryName" TEXT NOT NULL,
+
+    CONSTRAINT "ProductCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductModel" (
+    "id" SERIAL NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "modelName" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "ProductModel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Stock" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "allertLow" INTEGER NOT NULL,
+
+    CONSTRAINT "Stock_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Collections" (
+    "id" SERIAL NOT NULL,
+    "modelId" INTEGER NOT NULL,
+    "genderId" INTEGER NOT NULL,
+    "apparelId" INTEGER NOT NULL,
+
+    CONSTRAINT "Collections_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Gender" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Gender_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Apparel" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "Apparel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Sizes" (
+    "id" SERIAL NOT NULL,
+    "size" TEXT NOT NULL,
+
+    CONSTRAINT "Sizes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Design" (
+    "id" SERIAL NOT NULL,
+    "design" TEXT NOT NULL,
+
+    CONSTRAINT "Design_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "nickName" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_collectionId_key" ON "products"("collectionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_sizeId_key" ON "products"("sizeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_designId_key" ON "products"("designId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProductCategory_categoryName_key" ON "ProductCategory"("categoryName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Stock_productId_key" ON "Stock"("productId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Collections_modelId_key" ON "Collections"("modelId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Gender_name_key" ON "Gender"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Apparel_type_key" ON "Apparel"("type");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Sizes_size_key" ON "Sizes"("size");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Design_design_key" ON "Design"("design");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_nickName_key" ON "users"("nickName");
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "Collections"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_sizeId_fkey" FOREIGN KEY ("sizeId") REFERENCES "Sizes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Stock" ADD CONSTRAINT "Stock_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Collections" ADD CONSTRAINT "Collections_modelId_fkey" FOREIGN KEY ("modelId") REFERENCES "ProductModel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Collections" ADD CONSTRAINT "Collections_genderId_fkey" FOREIGN KEY ("genderId") REFERENCES "Gender"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Collections" ADD CONSTRAINT "Collections_apparelId_fkey" FOREIGN KEY ("apparelId") REFERENCES "Apparel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
