e8603c2a8c8dca24bef0c25374de50286bb29c92
RichardPMachado <richardmachado2211@gmail.com>
2023-11-20 18:18:21 -0300

    feat: content painel-administrativo/colaboradores

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -35,7 +35,7 @@ function NavBar() {
       <NavigationMenu.List className="flex row items-center">
         <NavigationMenu.Item>
           <NavigationMenu.Link
-            className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+            className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
             href="/menu"
           >
             Inicio
@@ -45,7 +45,7 @@ function NavBar() {
           <>
             <NavigationMenu.Item>
               <NavigationMenu.Link
-                className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+                className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
                 href="/novo-pedido"
                 >
                 Novo Pedido 
@@ -53,7 +53,7 @@ function NavBar() {
             </NavigationMenu.Item>
             <NavigationMenu.Item>
               <NavigationMenu.Link
-                className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+                className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
                 href="/produtos-em-falta"
               >
                 Pedidos em falta 
@@ -65,7 +65,7 @@ function NavBar() {
           <>
             <NavigationMenu.Item>
               <NavigationMenu.Link
-                className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+                className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
                 href="/pedidos-pendentes"
               >
                 Pedidos pendentes 
@@ -73,7 +73,7 @@ function NavBar() {
             </NavigationMenu.Item>
             <NavigationMenu.Item>
               <NavigationMenu.Link
-                className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+                className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
                 href="/reposição-de-produtos"
               >
                 Reposição de produtos
@@ -84,45 +84,80 @@ function NavBar() {
         {isAdmin && (
           <>
             <NavigationMenu.Item>
-              <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+              <NavigationMenu.Trigger className="group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
                 Painel Administrativo{' '}
                 <CaretDownIcon
-                  className="text-violet10 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                  className="relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
                   aria-hidden
                 />
               </NavigationMenu.Trigger>
-              <NavigationMenu.Content className="absolute top-0 left-0 w-full sm:w-auto">
+              <NavigationMenu.Content className="absolute top-0 left-0 w-full sm:w-auto bg-gray-600 rounded-2xl">
                 <ul className="m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[600px] sm:grid-flow-col sm:grid-rows-3">
-                  <li title="Colaboradores" >
-                    
+                  <li title="Colaboradores" >                   
                     <NavigationMenu.Link asChild>
                       <a
-                        className={'focus:shadow-[0_0_0_2px] focus:shadow-violet7 hover:bg-mauve3 block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
+                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
                         href="/painel-administrativo/colaboradores"
                       >
-                        <div className="text-violet12 mb-[5px] font-medium leading-[1.2]">Colaboradores</div>
-                        <p className="text-mauve11 leading-[1.4]">
+                        <div className="mb-[5px] font-medium leading-[1.2]">Colaboradores</div>
+                        <p className="leading-[1.4]">
                           Build high-quality, accessible design systems and web apps.
                         </p>
                       </a>
-                    </NavigationMenu.Link>                    
+                    </NavigationMenu.Link>
                   </li>
-
-                  {/* <ListItem title="Produtos" href="/painel-administrativo/colaboradores">
-                    A quick tutorial to get you up and running with Radix Primitives.
-                  </ListItem>
-                  <ListItem title="Styling" href="/primitives/docs/guides/styling">
-                    Unstyled and compatible with any styling solution.
-                  </ListItem>
-                  <ListItem title="Animation" href="/primitives/docs/guides/animation">
-                    Use CSS keyframes or any animation library of your choice.
-                  </ListItem>
-                  <ListItem title="Accessibility" href="/primitives/docs/overview/accessibility">
-                    Tested in a range of browsers and assistive technologies.
-                  </ListItem>
-                  <ListItem title="Releases" href="/primitives/docs/overview/releases">
-                    Radix Primitives releases and their changelogs.
-                  </ListItem> */}
+                  <li title="Produtos" >                   
+                    <NavigationMenu.Link asChild>
+                      <a
+                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
+                        href="/painel-administrativo/produtos"
+                      >
+                        <div className="mb-[5px] font-medium leading-[1.2]">Produtos</div>
+                        <p className="leading-[1.4]">
+                          Build high-quality, accessible design systems and web apps.
+                        </p>
+                      </a>
+                    </NavigationMenu.Link>
+                  </li>
+                  <li title="Lojas" >                   
+                    <NavigationMenu.Link asChild>
+                      <a
+                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
+                        href="/painel-administrativo/lojas"
+                      >
+                        <div className="mb-[5px] font-medium leading-[1.2]">Lojas</div>
+                        <p className="leading-[1.4]">
+                          Build high-quality, accessible design systems and web apps.
+                        </p>
+                      </a>
+                    </NavigationMenu.Link>
+                  </li>
+                  <li title="Valuation" >                   
+                    <NavigationMenu.Link asChild>
+                      <a
+                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
+                        href="/painel-administrativo/valuation"
+                      >
+                        <div className="mb-[5px] font-medium leading-[1.2]">Valuation</div>
+                        <p className="leading-[1.4]">
+                          Build high-quality, accessible design systems and web apps.
+                        </p>
+                      </a>
+                    </NavigationMenu.Link>
+                  </li>
+                  <li title="Vendas" >                   
+                    <NavigationMenu.Link asChild>
+                      <a
+                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
+                        href="/painel-administrativo/vendas"
+                      >
+                        <div className="mb-[5px] font-medium leading-[1.2]">Vendas</div>
+                        <p className="leading-[1.4]">
+                          Build high-quality, accessible design systems and web apps.
+                        </p>
+                      </a>
+                    </NavigationMenu.Link>
+                  </li>
                 </ul>
               </NavigationMenu.Content>
             </NavigationMenu.Item> 
@@ -135,15 +170,32 @@ function NavBar() {
             </NavigationMenu.Item> 
           </>
         )}
-        <NavigationMenu.Indicator className="data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-[1] flex h-[10px] items-end justify-center overflow-hidden transition-[width,transform_250ms_ease]">
+        <NavigationMenu.Indicator className="NavigationMenuIndicator">    
           <div className="relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-white" />
         </NavigationMenu.Indicator>        
       </NavigationMenu.List>
-      <div className="perspective-[2000px] absolute top-full left-0 flex w-full justify-center">
-        <NavigationMenu.Viewport className="data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]" />
-      </div>
+      {/* <div className="perspective-[2000px] absolute top-full left-0 flex w-full justify-center"> */}
+        <NavigationMenu.Viewport className="NavigationMenuViewport"/>
+      {/* </div> */}
     </NavigationMenu.Root>
+      // "data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]" />
+      // className="data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-[1] flex h-[10px] items-end justify-center overflow-hidden transition-[width,transform_250ms_ease]">
   );
 }
+
+// const Link = ({ href, ...props }: {href: string}) => {
+//   const router = useRouter();
+//   const isActive = router.asPath === href;
+
+//   return (
+//     <NextLink href={href} passHref legacyBehavior>
+//       <NavigationMenu.Link
+//         className="NavigationMenuLink"
+//         active={isActive}
+//         {...props}
+//       />
+//     </NextLink>
+//   );
+// };
 
 export default NavBar;
