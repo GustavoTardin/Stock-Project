21747b5101af8262a928f9ee78793118e2fed4ca
RichardPMachado <richardmachado2211@gmail.com>
2023-11-23 21:21:53 -0300

    refactor: painel-administrativo

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import AdminPanel from './Admin/AdminPanel';
 import { Theme } from './Theme';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { CaretDownIcon } from '@radix-ui/react-icons';
-import React from 'react';
+import React  from 'react';
 
 function NavBar() {
   const signOut = useSignOut();
@@ -28,57 +28,59 @@ function NavBar() {
   //   console.log(isDropdownOpen);
   // };
   
+  type LinkNavProps = {
+    href: string;
+    title: string;
+   };
+   
+   const LinkNav: React.FC<LinkNavProps> = ({ href, title }) => {
+    return (
+      <NavigationMenu.Item>
+        <NavigationMenu.Link
+          className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+          href={href}
+          title={title} 
+        >
+          {title}
+        </NavigationMenu.Link>
+      </NavigationMenu.Item>
+    )
+   }
+   const LinkNavLi: React.FC<LinkNavProps> = ({ href, title }) => {
+    return (
+      <li title={title} >                   
+        <NavigationMenu.Link asChild>
+          <a
+            className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
+            href={href}
+          >
+            <div className="mb-[5px] font-medium leading-[1.2]">{title}</div>
+            <p className="leading-[1.4]">
+              Build high-quality, accessible design systems and web apps.
+            </p>
+          </a>
+        </NavigationMenu.Link>
+      </li>
+    )
+   }
+  
 
   return (
   
     <NavigationMenu.Root className='pr-10'>
       <NavigationMenu.List className="flex row items-center">
-        <NavigationMenu.Item>
-          <NavigationMenu.Link
-            className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
-            href="/menu"
-          >
-            Inicio
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
+        <LinkNav title="Início" href="/menu"/>
         {storeAccess && (
           <>
-            <NavigationMenu.Item>
-              <NavigationMenu.Link
-                className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
-                href="/novo-pedido"
-                >
-                Novo Pedido 
-              </NavigationMenu.Link>
-            </NavigationMenu.Item>
-            <NavigationMenu.Item>
-              <NavigationMenu.Link
-                className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
-                href="/produtos-em-falta"
-              >
-                Pedidos em falta 
-              </NavigationMenu.Link>
-            </NavigationMenu.Item>          
+            <LinkNav title="Novo Pedido" href="/novo-pedido"/>
+            <LinkNav title="Pedidos em falta" href="/produtos-em-falta"/>        
           </>
         )}
         {stockAccess && (
           <>
-            <NavigationMenu.Item>
-              <NavigationMenu.Link
-                className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
-                href="/pedidos-pendentes"
-              >
-                Pedidos pendentes 
-              </NavigationMenu.Link>
-            </NavigationMenu.Item>
-            <NavigationMenu.Item>
-              <NavigationMenu.Link
-                className="block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
-                href="/reposição-de-produtos"
-              >
-                Reposição de produtos
-              </NavigationMenu.Link>
-            </NavigationMenu.Item>          
+            <LinkNav title="Pedidos pendentes" href="/pedidos-pendentes"/>        
+            <LinkNav title="Reposição de produtos" href="/reposição-de-produtos"/>        
+       
           </>
         )}
         {isAdmin && (
@@ -93,71 +95,11 @@ function NavBar() {
               </NavigationMenu.Trigger>
               <NavigationMenu.Content className="absolute top-0 left-0 w-full sm:w-auto bg-gray-600 rounded-2xl">
                 <ul className="m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[600px] sm:grid-flow-col sm:grid-rows-3">
-                  <li title="Colaboradores" >                   
-                    <NavigationMenu.Link asChild>
-                      <a
-                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
-                        href="/painel-administrativo/colaboradores"
-                      >
-                        <div className="mb-[5px] font-medium leading-[1.2]">Colaboradores</div>
-                        <p className="leading-[1.4]">
-                          Build high-quality, accessible design systems and web apps.
-                        </p>
-                      </a>
-                    </NavigationMenu.Link>
-                  </li>
-                  <li title="Produtos" >                   
-                    <NavigationMenu.Link asChild>
-                      <a
-                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
-                        href="/painel-administrativo/produtos"
-                      >
-                        <div className="mb-[5px] font-medium leading-[1.2]">Produtos</div>
-                        <p className="leading-[1.4]">
-                          Build high-quality, accessible design systems and web apps.
-                        </p>
-                      </a>
-                    </NavigationMenu.Link>
-                  </li>
-                  <li title="Lojas" >                   
-                    <NavigationMenu.Link asChild>
-                      <a
-                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
-                        href="/painel-administrativo/lojas"
-                      >
-                        <div className="mb-[5px] font-medium leading-[1.2]">Lojas</div>
-                        <p className="leading-[1.4]">
-                          Build high-quality, accessible design systems and web apps.
-                        </p>
-                      </a>
-                    </NavigationMenu.Link>
-                  </li>
-                  <li title="Valuation" >                   
-                    <NavigationMenu.Link asChild>
-                      <a
-                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
-                        href="/painel-administrativo/valuation"
-                      >
-                        <div className="mb-[5px] font-medium leading-[1.2]">Valuation</div>
-                        <p className="leading-[1.4]">
-                          Build high-quality, accessible design systems and web apps.
-                        </p>
-                      </a>
-                    </NavigationMenu.Link>
-                  </li>
-                  <li title="Vendas" >                   
-                    <NavigationMenu.Link asChild>
-                      <a
-                        className={'block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors'}
-                        href="/painel-administrativo/vendas"
-                      >
-                        <div className="mb-[5px] font-medium leading-[1.2]">Vendas</div>
-                        <p className="leading-[1.4]">
-                          Build high-quality, accessible design systems and web apps.
-                        </p>
-                      </a>
-                    </NavigationMenu.Link>
-                  </li>
+                  <LinkNavLi title="Colaboradores" href="/painel-administrativo/colaboradores" />
+                  <LinkNavLi title="Produtos" href="/painel-administrativo/produtos" />
+                  <LinkNavLi title="Lojas" href="/painel-administrativo/lojas" />
+                  <LinkNavLi title="Valuation" href="/painel-administrativo/valuation" />
+                  <LinkNavLi title="Vendas" href="/painel-administrativo/vendas"/>
                 </ul>
               </NavigationMenu.Content>
             </NavigationMenu.Item> 
