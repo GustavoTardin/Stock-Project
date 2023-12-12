500dabd572ca4aff66dd3ea820ca2129eedf6e43
RichardPMachado <richardmachado2211@gmail.com>
2023-12-11 23:12:50 -0300

    chores: useState has been renamed

--- app/frontend/src/components/Admin/User/creation/MultiSelect.tsx
+++ app/frontend/src/components/Admin/User/creation/MultiSelect.tsx
@@ -4,13 +4,13 @@ import { IOptionType } from '../../Store/types';
 import { getStoreNames } from '../../../../Utils/Requests/storeRequests';
 import TNewUser from './TypeNewUser';
 
-function MultiSelect({ userDataSetter, stores }: {
-  userDataSetter: React.Dispatch<React.SetStateAction<TNewUser>>,
+function MultiSelect({ setUserData: setUserData, stores }: {
+  setUserData: React.Dispatch<React.SetStateAction<TNewUser>>,
   stores: string[], }) {
-  const [storeNames, storeNamesSetter] = useState([]);
+  const [storeNames, setStoreNames] = useState([]);
 
   const handleMultiSelectChange = (selected: MultiValue<IOptionType>) => {
-    userDataSetter((prevData) => ({
+    setUserData((prevData) => ({
       ...prevData,
       stores: selected.map((option) => option.value),
     }));
@@ -24,28 +24,44 @@ function MultiSelect({ userDataSetter, stores }: {
   useEffect(() => {
     const getStores = async () => {
       const names = await getStoreNames();
-      storeNamesSetter(names);
+      setStoreNames(names);
     };
     getStores();
   }, []);
 
   return (
+    <div className='pt-10'>
+      <label htmlFor="stores" className='text-xl'>
+        Trabalha na(s) loja(s):
 
-    <label htmlFor="stores">
-      Trabalha na(s) loja(s):
+        <Select
+          className='w-[19.5rem]'
+          theme={(theme) => ({
+            ...theme,
+            colors: {
+              ...theme.colors,
+              primary:'black',
+            },
+            spacing: {
+              ...theme.spacing,
+              baseUnit:0,
+              controlHeight:34,
+              menuGutter:5
+            } 
+          })}
+          id="stores"
+          isMulti
+          placeholder="Lojas"
+          options={ options }
+          onChange={ (selected) => {
+            handleMultiSelectChange(selected);
+          } }
+          value={ options.filter((option) => stores.includes(option.value)) }
+          isClearable
+        />
+      </label>
+    </div>
 
-      <Select
-        id="stores"
-        isMulti
-        placeholder="Lojas"
-        options={ options }
-        onChange={ (selected) => {
-          handleMultiSelectChange(selected);
-        } }
-        value={ options.filter((option) => stores.includes(option.value)) }
-        isClearable
-      />
-    </label>
 
   );
 }
