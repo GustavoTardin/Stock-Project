f59ace97c581bc600cb41e873a6ca38ad7b437ea
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-12-22 13:32:14 -0300

    feat: add GET /stores/:id in storeRouter, that was already implemented

--- app/backend/src/api/Routes/storeRouter.ts
+++ app/backend/src/api/Routes/storeRouter.ts
@@ -4,11 +4,26 @@ import { Router } from 'express'
 import StoreService from '../Services/StoreService'
 import { storeModel } from '../Models'
 import StoreController from '../Controllers/StoreController'
+import { TokenValidation, UserValidation } from '../Middlewares'
+import credentialGuard from '../Utils/credentialGuard'
 
 const storeRouter = Router()
 const storeService = new StoreService(storeModel)
 const storeController = new StoreController(storeService)
+const { tokenRequired } = TokenValidation
+const { paramsIdRequired } = UserValidation
 
-storeRouter.get('/', storeController.getAll)
+storeRouter.get(
+  '/',
+  tokenRequired(credentialGuard.highLevelAccess),
+  storeController.getAll,
+)
+
+storeRouter.get(
+  '/:id',
+  tokenRequired(credentialGuard.freeAccess),
+  paramsIdRequired,
+  storeController.getById,
+)
 
 export default storeRouter
