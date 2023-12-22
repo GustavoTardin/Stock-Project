aea28b1fb7f15eef3f9b4ce915f4704a8ae764ca
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-12-22 14:26:22 -0300

    feat: develop create store funcionality in service layer

--- app/backend/src/api/Services/StoreService.ts
+++ app/backend/src/api/Services/StoreService.ts
@@ -1,10 +1,16 @@
+import ITransaction from '../Contracts/interfaces/prisma/ITransaction'
 import {
   IDbStore,
   ICreateStore,
   IStoreModel,
+  IStoreWithAddress,
 } from '../Contracts/interfaces/stores'
+import { newStoreSchema } from '../Contracts/zod/schemas/stores'
 import Store from '../Domains/Store'
+import DomainFactory from '../Utils/DomainFactory'
 import DomainTypes from '../Utils/DomainTypes'
+import validateField from '../Utils/serviceValidation'
+import prisma from '../database/prisma'
 import AbstractService from './AbstractService'
 
 class StoreService extends AbstractService<
@@ -17,8 +23,29 @@ class StoreService extends AbstractService<
     super(storeModel, DomainTypes.STORE)
   }
 
-  create(): Promise<Store> {
-    throw Error('not implemented')
+  async create(data: unknown): Promise<Store> {
+    const { store, address } = validateField<IStoreWithAddress>(
+      newStoreSchema,
+      data,
+    )
+    const newStoreId = await prisma.$transaction(
+      async (tx): Promise<number> => {
+        try {
+          const { id } = await this._model.create(store, tx as ITransaction)
+          if (address) {
+            await this.storeAddressModel.create(id, address, tx as ITransaction)
+          }
+          return id
+        } catch (error) {
+          console.log(error)
+          throw error
+        }
+      },
+    )
+    const includeInactive = false
+    const newStore = await this._model.getById(newStoreId, includeInactive)
+    const domain = DomainFactory.createDomain<Store>(this.domainName, newStore)
+    return domain
   }
 }
 
