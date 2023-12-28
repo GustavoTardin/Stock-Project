01f192b3f2c41c7544c4614b1cc271edd7402583
RichardPMachado <richardmachado2211@gmail.com>
2023-12-28 15:54:43 -0300

    style: absolute function has been implemented

--- app/frontend/src/components/Loading.tsx
+++ app/frontend/src/components/Loading.tsx
@@ -2,7 +2,7 @@ import {Loader2 } from "lucide-react"
 
 export function Loading() {
   return (
-    <div className="bg-blueDetails w-full h-10 flex items-center justify-center overflow-hidden">
+    <div className="absolute z-[1] bg-blueDetails w-full h-10 flex items-center justify-center overflow-hidden">
       <Loader2 width="10px" className="w-7 h-7 animate-spin"/>
     </div>
   )
