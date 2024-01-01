92c91177c0bc093241b0b3d8c3113dbb592d959b
RichardPMachado <richardmachado2211@gmail.com>
2024-01-01 16:23:39 -0300

    feat: pull origin main

---
+++ app/frontend/src/Service/Requests/salesRequests.ts
@@ -0,0 +1,9 @@
+import ISale from '../../components/Admin/Sales/interfaces/ISale';
+import { api } from './requests';
+
+const postSale = async (salesSummary: ISale) => {
+  const { data } = await api.post('/sales', salesSummary);
+  return data;
+};
+
+export { postSale };
---
+++ app/frontend/src/views/index.ts
@@ -0,0 +1,23 @@
+import CreateOrder from './Sales/CreateOrder';
+import Home from './Home';
+import Login from './Login';
+import LowStockItems from './LowStockItems';
+import PendingOrders from './WareHouse/PendingOrders';
+import StockRefill from './WareHouse/StockRefill';
+import Valuation from './Admin/Valuation';
+import UserManagement from './Admin/UserManagement';
+import StoreManagement from './Admin/StoreManagement';
+import ProductManagement from './Admin/ProductManagement';
+
+export {
+  CreateOrder,
+  Home,
+  Login,
+  LowStockItems,
+  PendingOrders,
+  StockRefill,
+  Valuation,
+  UserManagement,
+  StoreManagement,
+  ProductManagement,
+};
